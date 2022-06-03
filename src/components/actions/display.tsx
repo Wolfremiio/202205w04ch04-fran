a86a6f129a2c6a0e7269f03f736cb367e7fbdc8e
FranLobo <floboflores@gmail.com>
2022-06-03 10:31:04 +0200

    ADD function s

---
+++ src/components/actions/display.tsx
@@ -0,0 +1,13 @@
+import { useContext } from "react"
+import {PhoneContext} from "../../context/phone-context";
+
+ function Display(){
+
+
+    return(
+
+
+    )
+}
+
+export default Display;
---
+++ src/context/phone-context.ts
@@ -0,0 +1,18 @@
+
+import { createContext } from 'react';
+
+const initialContext: {
+    display: string;
+    addNumber: (number: string) => void;
+    calling: boolean;
+    startCall: () => void;
+
+} = {
+    display: '',
+    addNumber: () => {},
+    calling: false,
+    startCall: () => {},
+
+};
+
+export const PhoneContext = createContext(initialContext);
