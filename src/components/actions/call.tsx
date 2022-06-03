54991e8b2789013d148bf3161479df9ad08a29f4
FranLobo <floboflores@gmail.com>
2022-06-03 11:39:48 +0200

    ADD function in call.tsx

--- src/components/actions/call.tsx
+++ src/components/actions/call.tsx
@@ -3,7 +3,29 @@ import {PhoneContext} from "../../context/phone-context";
 
  function Call (){
 
+const {display, call, startCall}= useContext{PhoneContext};
+
+
+let state = 'call';
+if (display.length == 9)
+{
+    state = 'call active'
+}
+if(call){
+    state = 'call off'
+}
+
+
     return(
+
+        <>
+        
+        <a href="#" onClick={startCall} className={state} >Call</a>
+     
+      </>
+
+
+
 
     )
  }
